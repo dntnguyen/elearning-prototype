181641b43fb5297449125fcc69eda3529f01469b
dntnguyen <dntnguyen@outlook.com>
2023-09-29 15:45:46 +0700

    edit wrong comment EOF in lesson assign

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -123,29 +123,29 @@ export const MENU_ITEMS: NbMenuItem[] = [
   //     },
   //   ],
   // },
-  {
-    title: 'UI Features',
-    icon: 'keypad-outline',
-    link: '/pages/ui-features',
-    children: [
-      {
-        title: 'Grid',
-        link: '/pages/ui-features/grid',
-      },
-      {
-        title: 'Icons',
-        link: '/pages/ui-features/icons',
-      },
-      {
-        title: 'Typography',
-        link: '/pages/ui-features/typography',
-      },
-      {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields',
-      },
-    ],
-  },
+  // {
+  //   title: 'UI Features',
+  //   icon: 'keypad-outline',
+  //   link: '/pages/ui-features',
+  //   children: [
+  //     {
+  //       title: 'Grid',
+  //       link: '/pages/ui-features/grid',
+  //     },
+  //     {
+  //       title: 'Icons',
+  //       link: '/pages/ui-features/icons',
+  //     },
+  //     {
+  //       title: 'Typography',
+  //       link: '/pages/ui-features/typography',
+  //     },
+  //     {
+  //       title: 'Animated Searches',
+  //       link: '/pages/ui-features/search-fields',
+  //     },
+  //   ],
+  // },
   // {
   //   title: 'Modal & Overlays',
   //   icon: 'browser-outline',
